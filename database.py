fe3b559569a285500604b0796481f77bf08ddf5a
herbertglenny-bit <herbertglenny@gmail.com>
2025-12-17 21:07:22 +0100

    Update database.py

--- database.py
+++ database.py
@@ -688,5 +688,4 @@ graph TD
 6.  **Microcirculación: El "Último Kilómetro":** Cuando sea factible, la evaluación de la microcirculación puede ofrecer información vital sobre la adecuación de la perfusión tisular que no es visible a través de los parámetros macrohemodinámicos.""",
         "pdf_bytes": None 
     },
-
 ]
